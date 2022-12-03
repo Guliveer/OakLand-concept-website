d78c58a641acfc44e76ecc13db7d70893be8e5b6
Guliveer <oliwer.pawelski@gmail.com>
2022-12-03 12:26:41 +0100

    Initial release

---
+++ js/script.js
@@ -0,0 +1,539 @@
+"use strict";
+(function () {
+	// Global variables
+	var
+		userAgent = navigator.userAgent.toLowerCase(),
+		initialDate = new Date(),
+
+		$document = $(document),
+		$window = $(window),
+		$html = $("html"),
+		$body = $("body"),
+
+		isDesktop = $html.hasClass("desktop"),
+		isIE = userAgent.indexOf("msie") !== -1 ? parseInt(userAgent.split("msie")[1], 10) : userAgent.indexOf("trident") !== -1 ? 11 : userAgent.indexOf("edge") !== -1 ? 12 : false,
+		isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+		windowReady = false,
+		isNoviBuilder = false,
+		livedemo = true,
+
+		plugins = {
+			bootstrapModalDialog: $('.modal'),
+			counter: $(".counter"),
+			captcha: $('.recaptcha'),
+			copyrightYear: $(".copyright-year"),
+			isotope: $(".isotope-wrap"),
+			preloader: $(".preloader"),
+			rdNavbar: $(".rd-navbar"),
+			rdMailForm: $(".rd-mailform"),
+			rdInputLabel: $(".form-label"),
+			regula: $("[data-constraints]"),
+			swiper: $(".swiper-container"),
+			wow: $(".wow")
+		};
+
+	/**
+	 * @desc Check the element was been scrolled into the view
+	 * @param {object} elem - jQuery object
+	 * @return {boolean}
+	 */
+	function isScrolledIntoView ( elem ) {
+		if ( isNoviBuilder ) return true;
+		return elem.offset().top + elem.outerHeight() >= $window.scrollTop() && elem.offset().top <= $window.scrollTop() + $window.height();
+	}
+
+	// Initialize scripts that require a loaded page
+	$window.on('load', function () {
+		// Page loader & Page transition
+		if (plugins.preloader.length && !isNoviBuilder) {
+			pageTransition({
+				target: document.querySelector( '.page' ),
+				delay: 0,
+				duration: 500,
+				classIn: 'fadeIn',
+				classOut: 'fadeOut',
+				classActive: 'animated',
+				conditions: function (event, link) {
+					return !/(\#|callto:|tel:|mailto:|:\/\/)/.test(link) && !event.currentTarget.hasAttribute('data-lightgallery');
+				},
+				onTransitionStart: function ( options ) {
+					setTimeout( function () {
+						plugins.preloader.removeClass('loaded');
+					}, options.duration * .75 );
+				},
+				onReady: function () {
+					plugins.preloader.addClass('loaded');
+					windowReady = true;
+				}
+			});
+		}
+
+		// jQuery Count To
+		if ( plugins.counter.length ) {
+			for ( var i = 0; i < plugins.counter.length; i++ ) {
+				var
+					counter = $(plugins.counter[i]),
+					initCount = function () {
+						var counter = $(this);
+						if ( !counter.hasClass( "animated-first" ) && isScrolledIntoView( counter ) ) {
+							counter.countTo({
+								refreshInterval: 40,
+								speed: counter.attr("data-speed") || 1000,
+								from: 0,
+								to: parseInt(counter.text(), 10)
+							});
+							counter.addClass('animated-first');
+						}
+					};
+
+				$.proxy( initCount, counter )();
+				$window.on( "scroll", $.proxy( initCount, counter ) );
+			}
+		}
+
+		// Isotope
+		if ( plugins.isotope.length ) {
+			for ( var i = 0; i < plugins.isotope.length; i++ ) {
+				var
+					wrap = plugins.isotope[ i ],
+					filterHandler = function ( event ) {
+						event.preventDefault();
+						for ( var n = 0; n < this.isoGroup.filters.length; n++ ) this.isoGroup.filters[ n ].classList.remove( 'active' );
+						this.classList.add( 'active' );
+						this.isoGroup.isotope.arrange( { filter: this.getAttribute( "data-isotope-filter" ) !== '*' ? '[data-filter*="' + this.getAttribute( "data-isotope-filter" ) + '"]' : '*' } );
+					},
+					resizeHandler = function () {
+						this.isoGroup.isotope.layout();
+					};
+
+				wrap.isoGroup = {};
+				wrap.isoGroup.filters = wrap.querySelectorAll( '[data-isotope-filter]' );
+				wrap.isoGroup.node = wrap.querySelector( '.isotope' );
+				wrap.isoGroup.layout = wrap.isoGroup.node.getAttribute( 'data-isotope-layout' ) ? wrap.isoGroup.node.getAttribute( 'data-isotope-layout' ) : 'masonry';
+				wrap.isoGroup.isotope = new Isotope( wrap.isoGroup.node, {
+					itemSelector: '.isotope-item',
+					layoutMode: wrap.isoGroup.layout,
+					filter: '*',
+					columnWidth: ( function() {
+						if ( wrap.isoGroup.node.hasAttribute('data-column-class') ) return wrap.isoGroup.node.getAttribute('data-column-class');
+						if ( wrap.isoGroup.node.hasAttribute('data-column-width') ) return parseFloat( wrap.isoGroup.node.getAttribute('data-column-width') );
+					}())
+				} );
+
+				for ( var n = 0; n < wrap.isoGroup.filters.length; n++ ) {
+					var filter = wrap.isoGroup.filters[ n ];
+					filter.isoGroup = wrap.isoGroup;
+					filter.addEventListener( 'click', filterHandler );
+				}
+
+				window.addEventListener( 'resize', resizeHandler.bind( wrap ) );
+			}
+		}
+	});
+
+	// Initialize scripts that require a finished document
+	$(function () {
+		isNoviBuilder = window.xMode;
+
+		/**
+		 * @desc Toggle swiper videos on active slides
+		 * @param {object} swiper - swiper slider
+		 */
+		function toggleSwiperInnerVideos(swiper) {
+			var prevSlide = $(swiper.slides[swiper.previousIndex]),
+				nextSlide = $(swiper.slides[swiper.activeIndex]),
+				videos,
+				videoItems = prevSlide.find("video");
+
+			for (var i = 0; i < videoItems.length; i++) {
+				videoItems[i].pause();
+			}
+
+			videos = nextSlide.find("video");
+			if (videos.length) {
+				videos.get(0).play();
+			}
+		}
+
+		/**
+		 * @desc Toggle swiper animations on active slides
+		 * @param {object} swiper - swiper slider
+		 */
+		function toggleSwiperCaptionAnimation(swiper) {
+			var prevSlide = $(swiper.container).find("[data-caption-animate]"),
+				nextSlide = $(swiper.slides[swiper.activeIndex]).find("[data-caption-animate]"),
+				delay,
+				duration,
+				nextSlideItem,
+				prevSlideItem;
+
+			for (var i = 0; i < prevSlide.length; i++) {
+				prevSlideItem = $(prevSlide[i]);
+
+				prevSlideItem.removeClass("animated")
+					.removeClass(prevSlideItem.attr("data-caption-animate"))
+					.addClass("not-animated");
+			}
+
+
+			var tempFunction = function (nextSlideItem, duration) {
+				return function () {
+					nextSlideItem
+						.removeClass("not-animated")
+						.addClass(nextSlideItem.attr("data-caption-animate"))
+						.addClass("animated");
+					if (duration) {
+						nextSlideItem.css('animation-duration', duration + 'ms');
+					}
+				};
+			};
+
+			for (var i = 0; i < nextSlide.length; i++) {
+				nextSlideItem = $(nextSlide[i]);
+				delay = nextSlideItem.attr("data-caption-delay");
+				duration = nextSlideItem.attr('data-caption-duration');
+				if (!isNoviBuilder) {
+					if (delay) {
+						setTimeout(tempFunction(nextSlideItem, duration), parseInt(delay, 10));
+					} else {
+						tempFunction(nextSlideItem, duration);
+					}
+
+				} else {
+					nextSlideItem.removeClass("not-animated")
+				}
+			}
+		}
+
+		/**
+		 * @desc Attach form validation to elements
+		 * @param {object} elements - jQuery object
+		 */
+		function attachFormValidator(elements) {
+			// Custom validator - phone number
+			regula.custom({
+				name: 'PhoneNumber',
+				defaultMessage: 'Podany numer telefonu jest nieprawidłowy',
+				validator: function () {
+					if (this.value === '') return true;
+					else return /^(\+\d)?[0-9\-\(\) ]{5,}$/i.test(this.value);
+				}
+			});
+
+			for (var i = 0; i < elements.length; i++) {
+				var o = $(elements[i]), v;
+				o.addClass("form-control-has-validation").after("<span class='form-validation'></span>");
+				v = o.parent().find(".form-validation");
+				if (v.is(":last-child")) o.addClass("form-control-last-child");
+			}
+
+			elements.on('input change propertychange blur', function (e) {
+				var $this = $(this), results;
+
+				if (e.type !== "blur") if (!$this.parent().hasClass("has-error")) return;
+				if ($this.parents('.rd-mailform').hasClass('success')) return;
+
+				if ((results = $this.regula('validate')).length) {
+					for (i = 0; i < results.length; i++) {
+						$this.siblings(".form-validation").text(results[i].message).parent().addClass("has-error");
+					}
+				} else {
+					$this.siblings(".form-validation").text("").parent().removeClass("has-error")
+				}
+			}).regula('bind');
+
+			var regularConstraintsMessages = [
+				{
+					type: regula.Constraint.Required,
+					newMessage: "To pole jest wymagane."
+				},
+				{
+					type: regula.Constraint.Email,
+					newMessage: "Ten adres e-mail jest nieprawidłowy."
+				},
+				{
+					type: regula.Constraint.Numeric,
+					newMessage: "Tyko cyfry są dozwolone."
+				},
+				{
+					type: regula.Constraint.Selected,
+					newMessage: "Proszę wybrać opcję."
+				}
+			];
+
+
+			for (var i = 0; i < regularConstraintsMessages.length; i++) {
+				var regularConstraint = regularConstraintsMessages[i];
+
+				regula.override({
+					constraintType: regularConstraint.type,
+					defaultMessage: regularConstraint.newMessage
+				});
+			}
+		}
+
+		/**
+		 * @desc Check if all elements pass validation
+		 * @param {object} elements - object of items for validation
+		 * @param {object} captcha - captcha object for validation
+		 * @return {boolean}
+		 */
+		function isValidated(elements, captcha) {
+			var results, errors = 0;
+
+			if (elements.length) {
+				for (var j = 0; j < elements.length; j++) {
+
+					var $input = $(elements[j]);
+					if ((results = $input.regula('validate')).length) {
+						for (k = 0; k < results.length; k++) {
+							errors++;
+							$input.siblings(".form-validation").text(results[k].message).parent().addClass("has-error");
+						}
+					} else {
+						$input.siblings(".form-validation").text("").parent().removeClass("has-error")
+					}
+				}
+
+				if (captcha) {
+					if (captcha.length) {
+						return validateReCaptcha(captcha) && errors === 0
+					}
+				}
+
+				return errors === 0;
+			}
+			return true;
+		}
+
+		// Additional class on html if mac os.
+		if (navigator.platform.match(/(Mac)/i)) {
+			$html.addClass("mac-os");
+		}
+
+		// Adds some loosing functionality to IE browsers (IE Polyfills)
+		if (isIE) {
+			if (isIE === 12) $html.addClass("ie-edge");
+			if (isIE === 11) $html.addClass("ie-11");
+			if (isIE < 10) $html.addClass("lt-ie-10");
+			if (isIE < 11) $html.addClass("ie-10");
+		}
+
+		// BootstrapModalDialog
+		if (plugins.bootstrapModalDialog.length) {
+			for (var i = 0; i < plugins.bootstrapModalDialog.length; i++) {
+				var modalItem = $(plugins.bootstrapModalDialog[i]);
+
+				modalItem.on('hidden.bs.modal', $.proxy(function () {
+					var activeModal = $(this),
+						rdVideoInside = activeModal.find('video'),
+						youTubeVideoInside = activeModal.find('iframe');
+
+					if (rdVideoInside.length) {
+						rdVideoInside[0].pause();
+					}
+
+					if (youTubeVideoInside.length) {
+						var videoUrl = youTubeVideoInside.attr('src');
+
+						youTubeVideoInside
+							.attr('src', '')
+							.attr('src', videoUrl);
+					}
+				}, modalItem));
+			}
+		}
+
+		// Copyright Year (Evaluates correct copyright year)
+		if (plugins.copyrightYear.length) {
+			plugins.copyrightYear.text(initialDate.getFullYear());
+		}
+
+		// UI To Top
+		if (isDesktop && !isNoviBuilder) {
+			$().UItoTop({
+				easingType: 'easeOutQuad',
+				containerClass: 'ui-to-top fa fa-angle-up'
+			});
+		}
+
+		// RD Navbar
+		if (plugins.rdNavbar.length) {
+			var aliaces, i, j, len, value, values, responsiveNavbar;
+			aliaces = ["-", "-sm-", "-md-", "-lg-", "-xl-", "-xxl-"];
+			values = [0, 576, 768, 992, 1200, 1600];
+			responsiveNavbar = {};
+
+			for (i = j = 0, len = values.length; j < len; i = ++j) {
+				value = values[i];
+				if (!responsiveNavbar[values[i]]) {
+					responsiveNavbar[values[i]] = {};
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'layout')) {
+					responsiveNavbar[values[i]].layout = plugins.rdNavbar.attr('data' + aliaces[i] + 'layout');
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout')) {
+					responsiveNavbar[values[i]]['deviceLayout'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout');
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on')) {
+					responsiveNavbar[values[i]]['focusOnHover'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on') === 'true';
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height')) {
+					responsiveNavbar[values[i]]['autoHeight'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height') === 'true';
+				}
+
+				if (isNoviBuilder) {
+					responsiveNavbar[values[i]]['stickUp'] = false;
+				} else if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up')) {
+					responsiveNavbar[values[i]]['stickUp'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up') === 'true';
+				}
+
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset')) {
+					responsiveNavbar[values[i]]['stickUpOffset'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset');
+				}
+			}
+
+
+			plugins.rdNavbar.RDNavbar({
+				anchorNav: !isNoviBuilder,
+				stickUpClone: (plugins.rdNavbar.attr("data-stick-up-clone") && !isNoviBuilder) ? plugins.rdNavbar.attr("data-stick-up-clone") === 'true' : false,
+				responsive: responsiveNavbar,
+				callbacks: {
+					onStuck: function () {
+						var navbarSearch = this.$element.find('.rd-search input');
+
+						if (navbarSearch) {
+							navbarSearch.val('').trigger('propertychange');
+						}
+					},
+					onDropdownOver: function () {
+						return !isNoviBuilder;
+					},
+					onUnstuck: function () {
+						if (this.$clone === null)
+							return;
+
+						var navbarSearch = this.$clone.find('.rd-search input');
+
+						if (navbarSearch) {
+							navbarSearch.val('').trigger('propertychange');
+							navbarSearch.trigger('blur');
+						}
+
+					}
+				}
+			});
+
+
+			if (plugins.rdNavbar.attr("data-body-class")) {
+				document.body.className += ' ' + plugins.rdNavbar.attr("data-body-class");
+			}
+		}
+
+		// Swiper
+		if (plugins.swiper.length) {
+			for (var i = 0; i < plugins.swiper.length; i++) {
+				var s = $(plugins.swiper[i]);
+				var pag = s.find(".swiper-pagination"),
+					next = s.find(".swiper-button-next"),
+					prev = s.find(".swiper-button-prev"),
+					bar = s.find(".swiper-scrollbar"),
+					swiperSlide = s.find(".swiper-slide"),
+					autoplay = false;
+
+				for (var j = 0; j < swiperSlide.length; j++) {
+					var $this = $(swiperSlide[j]),
+						url;
+
+					if (url = $this.attr("data-slide-bg")) {
+						$this.css({
+							"background-image": "url(" + url + ")",
+							"background-size": "cover"
+						})
+					}
+				}
+
+				swiperSlide.end()
+					.find("[data-caption-animate]")
+					.addClass("not-animated")
+					.end();
+
+				s.swiper({
+					autoplay: !isNoviBuilder && $.isNumeric(s.attr('data-autoplay')) ? s.attr('data-autoplay') : false,
+					direction: s.attr('data-direction') ? s.attr('data-direction') : "horizontal",
+					effect: s.attr('data-slide-effect') ? s.attr('data-slide-effect') : "slide",
+					speed: s.attr('data-slide-speed') ? s.attr('data-slide-speed') : 600,
+					keyboardControl: s.attr('data-keyboard') === "true",
+					mousewheelControl: s.attr('data-mousewheel') === "true",
+					mousewheelReleaseOnEdges: s.attr('data-mousewheel-release') === "true",
+					nextButton: next.length ? next.get(0) : null,
+					prevButton: prev.length ? prev.get(0) : null,
+					pagination: pag.length ? pag.get(0) : null,
+					paginationClickable: pag.length ? pag.attr("data-clickable") !== "false" : false,
+					paginationBulletRender: pag.length ? pag.attr("data-index-bullet") === "true" ? function (swiper, index, className) {
+						return '<span class="' + className + '">' + (index + 1) + '</span>';
+					} : null : null,
+					scrollbar: bar.length ? bar.get(0) : null,
+					scrollbarDraggable: bar.length ? bar.attr("data-draggable") !== "false" : true,
+					scrollbarHide: bar.length ? bar.attr("data-draggable") === "false" : false,
+					loop: isNoviBuilder ? false : s.attr('data-loop') !== "false",
+					simulateTouch: s.attr('data-simulate-touch') && !isNoviBuilder ? s.attr('data-simulate-touch') === "true" : false,
+					onTransitionStart: function (swiper) {
+						toggleSwiperInnerVideos(swiper);
+					},
+					onTransitionEnd: function (swiper) {
+						toggleSwiperCaptionAnimation(swiper);
+					},
+					onInit: function (swiper) {
+						toggleSwiperInnerVideos(swiper);
+						toggleSwiperCaptionAnimation(swiper);
+					}
+				});
+			}
+		}
+
+		// WOW
+		if ($html.hasClass("wow-animation") && plugins.wow.length && !isNoviBuilder && isDesktop) {
+			new WOW().init();
+		}
+
+		// RD Input Label
+		if (plugins.rdInputLabel.length) {
+			plugins.rdInputLabel.RDInputLabel();
+		}
+
+		// Regula
+		if (plugins.regula.length) {
+			attachFormValidator(plugins.regula);
+		}
+
+		// Estimated price calculator
+		document.getElementById("width").addEventListener("change", calculateTotal);
+		document.getElementById("length").addEventListener("change", calculateTotal);
+		document.getElementById("floors").addEventListener("change", calculateTotal);
+
+		function calculateTotal() {
+			var width = document.getElementById("width").value;
+			var length = document.getElementById("length").value;
+			var floors = document.getElementById("floors").value;
+			var price = 115;
+			
+			if (floors > 1) {
+				var floors = parseInt(floors) - 0.13;
+			}
+			if (width == "" || length == "" || floors == "" || width < 1 || length < 1 || floors < 1 ) {
+				document.getElementById("estimated-price-total").innerHTML = "<font color='#f5543f'>Podaj wszystkie wymiary potrzebne do wyceny.</font>";
+			} else {
+				var total = parseInt(width) * parseInt(length) * floors * price;
+				total = Math.round(total);
+				document.getElementById("estimated-price-total").innerHTML = "Szacowany koszt budowy szkieletu: " + total + "&nbsp;zł";
+			}
+		}
+	});
+}());
+
+// Download file from attr, if .download is pressed. Download in new tab.
+$('.download').click(function () {
+    var file = $(this).attr('file');
+    window.open(file, '_blank');
+});
